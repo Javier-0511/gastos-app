13baf0ba6762c53782f501583c413a9690aad0c8
Javier-0511 <Javier-0511@users.noreply.github.com>
2026-05-31 15:16:30 +0000

    deploy: d19509fca0f43e455675c57ca9096c4a62c0d99c

--- service-worker-assets.js
+++ service-worker-assets.js
@@ -1,5 +1,5 @@
 self.assetsManifest = {
-  "version": "RnBeZjjC",
+  "version": "Ajcl3szq",
   "assets": [
     {
       "hash": "sha256-K8Nxatbbe+Vth29RgKhgu62Gnf7hgrnY4ifacCSDOuA=",
@@ -198,8 +198,8 @@ self.assetsManifest = {
       "url": "_framework/Blazor-ApexCharts.xli09jm79l.wasm"
     },
     {
-      "hash": "sha256-ecicX71Bx/uurb+2de6wlToT6VFoKPtBcYUhhw174lA=",
-      "url": "_framework/GastosApp.Client.35hbkmy1mr.wasm"
+      "hash": "sha256-eONYEBHNaCkl1nLOFWmsIMHVyRwGhMtTS+md6ZvKxwA=",
+      "url": "_framework/GastosApp.Client.0ybgbmg9cj.wasm"
     },
     {
       "hash": "sha256-nzUhTDC0UkiTVPNwQNQ14yv8bXbxdm3f9NA1O/98k90=",
@@ -454,8 +454,8 @@ self.assetsManifest = {
       "url": "_framework/System.ObjectModel.ete8oqbmu5.wasm"
     },
     {
-      "hash": "sha256-WgS42d4vizZ1x7RbDKKJMFBd6j+UurPx9Bkv558DCs0=",
-      "url": "_framework/System.Private.CoreLib.6xjq9156jk.wasm"
+      "hash": "sha256-crr8+MCsEr1VIoB0zk27SgCC9M7yOvJG7Je9tRC55OU=",
+      "url": "_framework/System.Private.CoreLib.aa4bs5wo6h.wasm"
     },
     {
       "hash": "sha256-78XvKu/k1z1Oh38rjYB9zVoJ6FtgsL/MRKA+6vo4jSA=",
@@ -574,16 +574,16 @@ self.assetsManifest = {
       "url": "_framework/blazor.webassembly.958z1vx7fr.js"
     },
     {
-      "hash": "sha256-RHdOieNiniYNIH+7Sq80mQO0xSQHH9o2idq7kORsjAA=",
-      "url": "_framework/dotnet.0tz88a65n6.js"
-    },
-    {
       "hash": "sha256-i4MH1ttKidpkFY/9i4kRe+7ux5JQMZds+qXuSkJqeog=",
       "url": "_framework/dotnet.native.f749u69f30.wasm"
     },
     {
       "hash": "sha256-95bE510j3EADMNW2OKa/DMBhu8N632dl5HGeBfXNEf4=",
       "url": "_framework/dotnet.native.rjbmzc4jpg.js"
+    },
+    {
+      "hash": "sha256-08Dnr9c9Bvpf+rOR+l9RKj5cCH1syz0AQ7tyhyll+Ws=",
+      "url": "_framework/dotnet.nn8o376vzm.js"
     },
     {
       "hash": "sha256-YyudibIWETMKrLb+nAZdJ+xDY7HY6BWf6s32UAdcvCU=",
@@ -618,7 +618,7 @@ self.assetsManifest = {
       "url": "icon.svg"
     },
     {
-      "hash": "sha256-8rY1RAEBMXilndqVfMufuc5ywUAenTSLrZTNgpksBUE=",
+      "hash": "sha256-zYkaq5zuO/XRTAfAEVTQGk0JAEfZLGnXJA7IAlyjqME=",
       "url": "index.html"
     },
     {
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-/* Manifest version: RnBeZjjC */
+/* Manifest version: Ajcl3szq */
 // Caution! Be sure you understand the caveats before publishing an application with
 // offline support. See https://aka.ms/blazor-offline-considerations
 
